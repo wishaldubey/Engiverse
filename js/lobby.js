aef21f552f301830e8c1b6740a82601e31bbffd0
VISHAL DUBEY <DUBEYVISHAL917@GMAIL.COM>
2024-03-08 06:35:12 +0530

    Update lobby.js

--- js/lobby.js
+++ js/lobby.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+/* import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-analytics.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 
@@ -50,4 +50,23 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     window.location = `room.html?room=${inviteCode}`
   });
-});
+}); */
+
+let form = document.getElementById('lobby__form')
+
+let displayName = sessionStorage.getItem('display_name')
+if(displayName){
+    form.name.value = displayName
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    sessionStorage.setItem('display_name', e.target.name.value)
+
+    let inviteCode = e.target.room.value
+    if(!inviteCode){
+        inviteCode = String(Math.floor(Math.random() * 10000))
+    }
+    window.location = `room.html?room=${inviteCode}`
+})
