73f043b5f350d070fadd57f98bc2c87c91552205
VISHAL DUBEY <DUBEYVISHAL917@GMAIL.COM>
2024-03-08 06:30:29 +0530

    Update lobby.js

--- js/lobby.js
+++ js/lobby.js
@@ -42,6 +42,12 @@ document.addEventListener('DOMContentLoaded', () => {
   // Handle form submission
   form.addEventListener('submit', (e) => {
     e.preventDefault();
-    // Perform form submission logic here
+    sessionStorage.setItem('display_name', e.target.name.value)
+
+    let inviteCode = e.target.room.value
+    if(!inviteCode){
+        inviteCode = String(Math.floor(Math.random() * 10000))
+    }
+    window.location = `room.html?room=${inviteCode}`
   });
 });
